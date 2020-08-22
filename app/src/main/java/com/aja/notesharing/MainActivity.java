8abe4253359c86d7834196dac9c0e575d95f0305
Jodiss Tribhu <jodiss.tri@gmail.com>
2020-08-22 21:44:30 +0530

    Commit 3 Login Page UI title bar removed

--- app/src/main/java/com/aja/notesharing/MainActivity.java
+++ app/src/main/java/com/aja/notesharing/MainActivity.java
@@ -2,14 +2,26 @@ package com.aja.notesharing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
