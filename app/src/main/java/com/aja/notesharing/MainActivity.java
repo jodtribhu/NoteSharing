11fe0ef016b406b8101de2fff63afb8cd91ac5a7
Jodiss Tribhu <jodiss.tri@gmail.com>
2020-08-24 22:00:21 +0530

    Commit 7 Creation of prototype of navigationBar

--- app/src/main/java/com/aja/notesharing/MainActivity.java
+++ app/src/main/java/com/aja/notesharing/MainActivity.java
@@ -1,26 +1,36 @@
 package com.aja.notesharing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.dashboard);
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, SignInActivity.class);
-                startActivity(intent);
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.about_app:startActivity(new Intent(getApplicationContext(),SearchNotes.class));
+                    case R.id.profile:startActivity(new Intent(getApplicationContext(),Add_notes.class));
+                }
             }
         });
+
     }
 }
