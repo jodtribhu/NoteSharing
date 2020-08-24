3bf5a629d21f1e8ac621d84d7ec6608c56978687
Jodiss Tribhu <jodiss.tri@gmail.com>
2020-08-24 20:49:33 +0530

    Commit 6 Proper Navigation between Activities

--- app/src/main/java/com/aja/notesharing/SignUpActivity.java
+++ app/src/main/java/com/aja/notesharing/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.aja.notesharing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void Back_to_sign_in(View view) {
+        Intent to_sign_in_intent=new Intent(SignUpActivity.this,SignInActivity.class);
+        startActivity(to_sign_in_intent);
     }
 }
